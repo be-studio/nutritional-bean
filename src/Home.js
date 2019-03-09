4f4076277ec3b8a8c57c3c65768bd928e3a3cd14
BE <hello@itsbe.studio>
2019-03-09 14:14:14 +0000

    Continued static build of home page.

--- src/Home.js
+++ src/Home.js
@@ -23,10 +23,44 @@ export class Home extends Component {
               <p className="home_txt_intro">
                 a registered Nutritional Therapist, Health Coach, wife and mother. I work with adults and children to restore health and wellbeing. My personal/primary mission is to educate, motivate and support YOU to make diet and lifestyle changes so that work for YOU.
               </p>
+
+              <p>
+                <a href="#" title="Keep reading">Keep reading&nbsp;&#8594;</a>
+              </p>
+            </div>
+
+            <div className="home_ctr_accreditations">
+
             </div>
 
             <div>
               <img src="assets/harriet-black-white.jpg" onMouseOver={e => (e.currentTarget.src = "assets/harriet.jpg")} onMouseOut={e => (e.currentTarget.src = "assets/harriet-black-white.jpg")} alt="Bean" />
+            </div>
+          </div>
+        </div>
+
+        <div className="home_ctr_second-image"></div>
+
+        <div className="home_ctr_marquee"></div>
+
+        <div className="home_ctr_bottom-panel">
+          <div>
+            <div>
+              <img src="/assets/tnb-journey.jpg" alt="" />
+            </div>
+
+            <div>
+              <p>
+                True healing is<br />
+                a journey &mdash;<br />
+                Let me guide you!
+              </p>
+
+              <p>
+                <img className="home_img_bean" src="/assets/bean.svg" />
+              </p>
+
+              <a href="#" title="Get in touch">Get in touch&nbsp;&#8594;</a>
             </div>
           </div>
         </div>
