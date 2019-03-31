964c8c39a6e34af6870e384af3cf5de493e24531
BE <hello@itsbe.studio>
2019-03-31 16:30:04 +0100

    Footer content changes.

--- src/Footer.js
+++ src/Footer.js
@@ -6,14 +6,29 @@ import { NewsletterSignUp } from "./NewsletterSignUp";
 export function Footer() {
   return (
     <div className="footer_ctr">
-      <div>
+      <div className="footer_ctr_top">
         <div>
-          <FooterMenu />
-        </div>
+          <div>
+            <FooterMenu />
+          </div>
 
-        <div>
-          <NewsletterSignUp />
+          <div>
+            <NewsletterSignUp />
+          </div>
         </div>
+      </div>
+
+
+      <div className="footer_ctr_accreditations">
+        <ul>
+          <li>
+            Photos: <a className="_mod_no-style" href="https://www.instagram.com/hananoguchi" target="_blank" rel="noopener noreferrer" title="Hana Noguchi">Hana Noguchi</a>
+          </li>
+
+          <li>
+            Illustrations: <a className="_mod_no-style" href="https://www.instagram.com/chamarrant" target="_blank" rel="noopener noreferrer" title="Maureen Valfort">Maureen Valfort</a>
+          </li>
+        </ul>
       </div>
     </div>
   );
