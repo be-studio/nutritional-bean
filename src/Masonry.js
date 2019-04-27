87852ede9427219161bc430dd3b1a5fcae35c288
BE <hello@itsbe.studio>
2019-04-27 10:28:07 +0100

    Attempts at blog masonry layout.

---
+++ src/Masonry.js
@@ -0,0 +1,46 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+Masonry.propTypes = {
+  columns: PropTypes.number.isRequired,
+  gap: PropTypes.number.isRequired,
+  children: PropTypes.arrayOf(PropTypes.element)
+};
+
+Masonry.defaultProps = {
+  columns: 2,
+  gap: 20,
+};
+
+
+export function Masonry(props) {
+  const columnWrapper = {};
+  const result = [];
+
+  for(let i = 0; i < props.columns; i++) {
+    columnWrapper[`column${i}`] = [];
+  }
+
+  props.children.forEach((child, index) => {
+    const columnIndex = index % props.columns;
+    columnWrapper[`column${columnIndex}`].push(
+      <div style={{ marginBottom: `${props.gap}px`}}>
+        {child}
+      </div>
+    );
+  });
+
+  for(let i = 0; i < props.columns; i++) {
+    result.push(
+      <div style={{ marginLeft: `${i > 0 ? props.gap : 0}px`, flex: 1 }}>
+        {columnWrapper[`column${i}`]}
+      </div>
+    );
+  }
+
+  return (
+    <div style={{ display: 'flex' }}>
+      {result}
+    </div>
+  );
+}
