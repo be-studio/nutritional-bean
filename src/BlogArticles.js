121d122eb83a085f9b95114d3d88343dde0a6638
BE <hello@its-be-studio.com>
2020-01-17 09:54:31 +0000

    1. Installed `Masonic` library; 2. Added masonry grid.

--- src/BlogArticles.js
+++ src/BlogArticles.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Masonry } from "./Masonry";
+import { Masonry } from "masonic";
 import { BlogItem } from "./BlogItem";
 import { Loader } from "./Loader";
 
@@ -15,6 +15,7 @@ export class BlogArticles extends Component {
 
   componentWillUpdate(nextProps, nextState, nextContext) {
     if(nextProps.articlesRecipes != this.state.articlesRecipes) {
+
       this.setState({
         articlesRecipes: nextProps.articlesRecipes
       });
@@ -36,26 +37,34 @@ export class BlogArticles extends Component {
 
 
   render() {
-    if(this.state.articlesRecipes) {
-      const blogArticles = this.state.articlesRecipes.map((articleRecipe, index) => {
+    console.log(this.state.articlesRecipes);
+    if(this.state.articlesRecipes && this.state.articlesRecipes.length !== 0) {
+      const blogArticles = this.state.articlesRecipes;
+
+      /*const blogArticles = this.state.articlesRecipes.map((articleRecipe, index) => {
         //noinspection ThisExpressionReferencesGlobalObjectJS
         return (
           <BlogItem key={index} itemKey={index} index={index} title={articleRecipe.title} permalink={articleRecipe.permalink} poster={articleRecipe.poster} excerpt={articleRecipe.excerpt} categories={articleRecipe.categories} updated={this.formatDate(articleRecipe.updated)} type={articleRecipe.type} />
         );
-      });
+      });*/
+
+      /*const masonryCard = ((index, articleRecipe) => (
+          <BlogItem key={index} itemKey={index} index={index} title={articleRecipe.title} permalink={articleRecipe.permalink} poster={articleRecipe.poster} excerpt={articleRecipe.excerpt} categories={articleRecipe.categories} updated={this.formatDate(articleRecipe.updated)} type={articleRecipe.type} />
+        )
+      );*/
+
+      const MasonryCard = ({index, data: articleRecipe, width}) => (
+        <BlogItem key={index} itemKey={index} index={index} title={articleRecipe.title} permalink={articleRecipe.permalink} poster={articleRecipe.poster} excerpt={articleRecipe.excerpt} categories={articleRecipe.categories} updated={this.formatDate(articleRecipe.updated)} type={articleRecipe.type} />
+      );
 
       return (
         <>
           <div className="blog-articles_ctr">
-            <Masonry gap={60}>
-              {blogArticles}
-            </Masonry>
+            <Masonry items={blogArticles} render={MasonryCard} />
           </div>
 
           <div className="blog-articles_ctr_mobile">
-            <Masonry columns={1}>
-              {blogArticles}
-            </Masonry>
+            <Masonry items={blogArticles} render={MasonryCard} />
           </div>
         </>
       );
